dca7b8781e340a1d3fadca77aa81c55c9f439b08
ethan <ethan,hodys@gmail.com>
2020-04-23 15:38:45 -0400

    Updated csv file name

--- finalr.base.r
+++ finalr.base.r
@@ -1,4 +1,14 @@
 library(dlookr)
 library(dplyr)
 
-data.raw=read.csv("dm2finaldataset.csv")
+data.raw=read.csv("cleandata_final.csv")
+
+data.raw %>% count(listens < 0)
+
+data.clean <- data.raw
+data.clean$listens[data.clean$listens < 0] <- 0
+data.clean %>% count(listens < 0)
+
+
+
+ 
